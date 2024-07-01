3aef88b0bce7caa6842a145c395fedd603fc16d9
itzdhwanil <itzdhwanil@gmail.com>
2024-07-01 11:56:45 +0000

    Completed quiz 1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(num:u32) -> u32 {
+    let order_value = if num > 40{
+        num
+    }
+    else{
+        num * 2
+    };
+    order_value
+}
 
 // Don't modify this function!
 #[test]
