7964c1e85345d24e55644d5118cefd92a36b81b8
itzdhwanil <itzdhwanil@gmail.com>
2024-07-11 17:36:25 +0000

    Completed 09_strings excercise

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -2,22 +2,21 @@
 //
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
-
-// I AM NOT DONE
-
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    input.trim().to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There are multiple ways to do this!
-    ???
+    let mut res = input.to_string();
+    res.push_str(" world!");
+    res
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    input.replace("cars", "balloons")
 }
 
 #[cfg(test)]
